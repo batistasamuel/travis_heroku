42bf0a106bffd9f4e63d0ed7218b44c77ae74aca
Samuel Batista <samuel.faeng@gmail.com>
2020-03-23 12:49:09 -0300

    fix src > config > index

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,16 @@
-#!/usr/bin/env node
+require('dotenv').config({path: 'variables.env'})
 
-
-const { app } = require('../app')
-
-app.listen(8080, function () {
-  console.info('%s listening at port %s', app.name, 8080)
-})
+module.exports = {
+    dialect:process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: `sentinel_log_${process.env.NODE_ENV}`,
+    port:process.env.DB_PORT,
+    define:{
+        timestamps:true,
+    },
+    dialectOptions:{
+        timezone:'Etc/GMT0',
+    },
+}
