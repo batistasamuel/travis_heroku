7580f4acb8d9773da0969a319f5498f9ff964ebd
Samuel Batista <samuel.faeng@gmail.com>
2020-03-23 15:41:26 -0300

    teste database name

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,8 @@ module.exports = {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: `sentinel_log_${process.env.NODE_ENV}`,
+    //database: `sentinel_log_${process.env.NODE_ENV}`,
+    database: 'sxl4osqu2x0gv2x8',
     port:process.env.DB_PORT,
     define:{
         timestamps:true,
