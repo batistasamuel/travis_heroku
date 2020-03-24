d6f2684c34001873b26ebfe7ad4153b5bfd89c9c
Samuel Batista <samuel.faeng@gmail.com>
2020-03-23 22:56:18 -0300

    database changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,19 @@
 require('dotenv').config({path: 'variables.env'})
 
+let Database
+
+if (NODE_ENV=='production'){
+    Database = 'ft48eyekd474dndk'
+} else {
+    Database = `sentinel_log_${process.env.NODE_ENV}`
+}
+
 module.exports = {
     dialect:process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: `sentinel_log_${process.env.NODE_ENV}`,
+    database: Database,
     port:process.env.DB_PORT,
     define:{
         timestamps:true,
