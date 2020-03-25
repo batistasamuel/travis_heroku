95ba1a57c502d92d949b31d46981f8dc2b73bb3b
Samuel Batista <samuel.faeng@gmail.com>
2020-03-25 10:24:55 -0300

    config deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,19 +1,11 @@
-require('dotenv').config({path: 'variables.env'})
-
-let Database
-
-if (`${process.env.NODE_ENV}` !== 'prod'){
-    Database = `sentinel_log_${process.env.NODE_ENV}`
-} else {
-    Database = process.env.DB_DATABASE
-}
+//require('dotenv').config({path: 'variables.env'})
 
 module.exports = {
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: Database,
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
     logging:false,
     define:{
