1313a8e2932f30a8c7064aa060a1454b1c244a33
Samuel Batista <samuel.faeng@gmail.com>
2020-03-24 23:34:48 -0300

    alteraçoes testes

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,6 @@
-// require('dotenv').config({path: 'variables.env'})
-
-const aws = require('aws-sdk');
+require('dotenv').config({path: 'variables.env'})
 
 let Database
-// const aws = new aws.S3({
-//     dialect: process.env.DB_DIALECT,
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: Database,
-//     port: process.env.DB_PORT,
-//     define:{
-//         timestamps:true,
-//     },
-//     dialectOptions:{
-//         timezone:'Etc/GMT0',
-//     },
-// })
 
 if (`${process.env.NODE_ENV}` !== 'prod'){
     Database = `sentinel_log_${process.env.NODE_ENV}`
@@ -24,7 +8,7 @@ if (`${process.env.NODE_ENV}` !== 'prod'){
     Database = process.env.DB_DATABASE
 }
 
-module.exports = new aws.S3({
+module.exports = {
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
@@ -37,4 +21,4 @@ module.exports = new aws.S3({
     dialectOptions:{
         timezone:'Etc/GMT0',
     },
-})
+}
