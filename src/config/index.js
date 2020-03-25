c981bb4426c8f4da7efee0b808ed6e2bd3ee87b3
Samuel Batista <samuel.faeng@gmail.com>
2020-03-24 23:14:34 -0300

    Co-authored-by: Rogerio Miguel <rooh-gerio@hotmail.com>

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,22 @@
-require('dotenv').config({path: 'variables.env'})
+// require('dotenv').config({path: 'variables.env'})
+
+const aws = require('aws-sdk');
 
 let Database
+// const aws = new aws.S3({
+//     dialect: process.env.DB_DIALECT,
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: Database,
+//     port: process.env.DB_PORT,
+//     define:{
+//         timestamps:true,
+//     },
+//     dialectOptions:{
+//         timezone:'Etc/GMT0',
+//     },
+// })
 
 if (`${process.env.NODE_ENV}` !== 'prod'){
     Database = `sentinel_log_${process.env.NODE_ENV}`
@@ -8,7 +24,7 @@ if (`${process.env.NODE_ENV}` !== 'prod'){
     Database = process.env.DB_DATABASE
 }
 
-module.exports = {
+module.exports = new aws.S3({
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
@@ -21,4 +37,4 @@ module.exports = {
     dialectOptions:{
         timezone:'Etc/GMT0',
     },
-}
+})
